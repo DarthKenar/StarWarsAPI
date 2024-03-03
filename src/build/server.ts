2f319d89fb3826698810e84667e758d897d0c16c
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-03 16:11:07 -0300

    index

--- src/build/server.ts
+++ src/build/server.ts
@@ -1,15 +1,26 @@
 import { Request, Response } from 'express';
 
 const path = require("path")
+const EventEmitter = require("events")
 const express = require('express')
 const app = express()
 const port = 3000
 
+const requestStarWarsAPI = new EventEmitter()
+
+//Events Handlers
+requestStarWarsAPI.on("refillDB",()=>{
+  console.log("Event refillDB")
+  //si db no existe
+  //realizar peticion a la API
+})
+
 const htmlFile = (file: string): string => {
-  return path.join(__dirname, '../public', file);
+  return path.join(__dirname, '../public', file)
 };
 
 app.get('/', (req:Request, res:Response) => {
+  requestStarWarsAPI.emit("refillDB")
   res.sendFile(htmlFile("index.html"))
 })
 
