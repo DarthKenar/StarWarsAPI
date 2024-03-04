30045e8dc37b74a173368eadbc3246d3770f84c1
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-04 16:06:57 -0300

    📦 Configuration:  Handlebars

--- src/build/db.ts
+++ src/build/db.ts
@@ -1,12 +1,1 @@
-import { Pool } from 'pg';
-
-//Configuracion local
-const pool = new Pool({
-  user: 'starwars',
-  host: 'localhost',
-  database: 'STARWARSAPI',
-  password: 'starwars',
-  port: 5432,
-});
-
-export default pool;
+//realizar conexion con base de datos SQLite
--- src/build/server.ts
+++ src/build/server.ts
@@ -1,29 +1,36 @@
 import { Request, Response } from 'express';
-let ejs = require('ejs'); //EJS, Handlebars o Pug
-let people = ['geddy', 'neil', 'alex'];
-let html = ejs.render('<%= people.join(", "); %>', {people: people});
+import { engine } from 'express-handlebars';
+import express from 'express';
+
 const AXIOS = require("axios")
 const PATH = require("path")
-const EXPRESS = require('express')
-const APP = EXPRESS()
 const PORT = 3000
 const EventEmitter = require("events")
+const app = express()
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', PATH.join(__dirname, './views'));
 
 const requestStarWarsAPI = new EventEmitter()
 
 async function checkDB(res:Response) {
-  //reemplazar true por comprobacion de base de datos
+  // Condicional, existe información de peliculas en la base de datos.
   if(false){
     console.log("updating DB")
   }else{
     try {
       var dataAPI = await AXIOS.get('https://swapi.dev/api/');
-      return dataAPI;
+      console.log("Informacion obtenida de 'https://swapi.dev/api/' correctamente.");
       //Guardar info en la base de datos
     } catch (error) {
       console.error(error);
-      res.status(500).end("Error al realizar la solicitud a la API externa");
-
+      res.statusCode = 501;
+      console.error(res.statusCode)
+      res.render("error",{
+        error: `${res.statusCode}`,
+        errorInfo: 'Bad Gateway'
+      })
     }
   }
 }
@@ -32,35 +39,38 @@ const htmlFile = (file: string): string => {
   return PATH.join(__dirname, '../public', file)
 };
 
-APP.use(async (req:Request, res:Response) => {
+app.use(async (req:Request, res:Response) => {
+  console.log(`Metodo: ${req.method}`)
+  console.log(`Path: ${req.path}`)
   switch (req.method) {
     case "GET":
       console.log(req.path)
       switch (req.path) {
         case "/":
           checkDB(res);
-          res.sendFile(htmlFile("index.html"));
-          break;
+          res.render("home",{})
+          break
         case "/entrar":
-          var dataAPI = checkDB(res);
-          res.render(htmlFile("data.html"),{data: html})
+          let dataAPI = await AXIOS.get('https://swapi.dev/api/');
+          console.log(dataAPI)
+          res.render("info",dataAPI)
           break;
         default:
           break;
       }
       break;
     case "DELETE":
-    
       break;
 
     default:
-      console.error(501)
+      res.statusCode = 501;
+      console.error(res.statusCode)
       res.status(501).end(htmlFile("index.html"))
       
   }
 })
 
-APP.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`)
 })
 
