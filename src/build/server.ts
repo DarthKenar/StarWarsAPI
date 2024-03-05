658087a09b62653cbbc2947bb360f856fddf03d5
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-05 18:19:49 -0300

    🚧 Work in Progress:  node:internal/process/promises:289             triggerUncaughtException(err, true /* fromPromise */);

--- src/build/server.ts
+++ src/build/server.ts
@@ -11,8 +11,16 @@ const PATH = require("path")
 const PORT = process.env.PORT || 3000
 const EventEmitter = require("events")
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', PATH.join(__dirname, '../views'));
+
+const htmlFile = (file: string): string => {
+  return PATH.join(__dirname, '../public', file);
+};
+
 function sendError(res:Response, codeError:number, errorInfo:string){
-  // console.error(res.statusCode);
+  console.error(res.statusCode);
   res.statusCode = codeError;
   res.render("errorTemplate",{
     error: `${codeError}`,
@@ -20,59 +28,79 @@ function sendError(res:Response, codeError:number, errorInfo:string){
   });
 };
 
+async function refillDB(res:Response){
+  try {
+    var linksAPI = await AXIOS.get('https://swapi.dev/api/');
+    var filmsAPI = await AXIOS.get(linksAPI.data.films);
+    for(let i = 0; i < filmsAPI.data.results.length; i++){
+      let filmAPI = filmsAPI.data.results[i]
+      let film = new Films()
+      film.title = filmAPI.title
+      film.episode_id = filmAPI.episode_id
+      await AppDataSource.manager.save(film)
+      //busco todos los persojaes de esa pelicula y los guardo en la base de datos. al finalizar 
+      //Si ya existe el personaje entonces simplemente agrego a su lista ManyToMany de peliculas la pelicula actual
+      console.log("Longitud de la cantidad de people en la película --", filmAPI.characters.length)
+      for(let j = 0; j < filmAPI.characters.length; j++){
+        let characterAPI = await AXIOS.get(filmAPI.characters[j]);
+        console.log(characterAPI.data)
+        let peopleRepository = await AppDataSource.getRepository(People)
+        let characterDB = await peopleRepository.findOneBy({name: characterAPI.data.name})
+        if(characterDB){
+          //Si existe entonces solo agregar la pelicula a su repertorio
+          characterDB.films.push(film)
+          film.characters.push(characterDB)
+        }else{
+          let people = new People()
+          people.name = characterAPI.data.name
+          people.gender = characterAPI.data.gender
+          people.species = await AXIOS.get(characterAPI.data.species[0]).data.name
+          people.films = [film]
+          await AppDataSource.manager.save(people)
+          console.log("PERSONAJE GUARDADO")
+          console.log(people.name)
+          film.characters.push(people)
+        }
+      };
+      await AppDataSource.manager.save(film)
+      console.log("PELICULA GUARDADA")
+      console.log(film.title)
+    }
+  } catch (error) {
+    sendError(res,502,'Bad Gateway');
+  }
+}
+
 async function checkDB(res:Response) {
   /*Chequea la base de datos y si no tiene películas las completa con la API externa*/
   // Condicional, existe información de peliculas en la base de datos.
   //Obtener lista de peliculas
   const filmsRepository = AppDataSource.getRepository(Films)
   const savedFilms = await filmsRepository.find()
-  
-  console.log("SavedFilms -->", {savedFilms})
-  console.log("SavedFilms -->", savedFilms)
-
-  if(savedFilms == null){
-    console.log("updating DB");
-  }else{
-    try {
-      console.log("SavedFilms -->", {savedFilms})
-      console.log("SavedFilms -->", savedFilms)
-      var dataAPI = await AXIOS.get('https://swapi.dev/api/films');
-      // console.log(typeof dataAPI)
-      dataAPI = dataAPI.data
-      // console.log("Informacion obtenida de 'https://swapi.dev/api/' correctamente.");
-      //Guardar info en la base de datos
-    } catch (error) {
-      sendError(res,502,'Bad Gateway');
-    }
+  if(savedFilms.length === 0){
+    refillDB(res);
   }
 }
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', PATH.join(__dirname, './views'));
-
-const htmlFile = (file: string): string => {
-  return PATH.join(__dirname, '../public', file);
-};
 
 AppDataSource.initialize()
   .then(() => {
     app.use(async (req:Request, res:Response) => {
-      // console.log(`Metodo: ${req.method}`);
-      // console.log(`Path: ${req.path}`);
+      console.log(`Metodo: ${req.method}`);
+      console.log(`Path: ${req.path}`);
       switch (req.method) {
         case "GET":
-          // console.log(req.path);
-          // console.log(req.params)
+          console.log("req.path: ", req.path);
+          console.log("req.params: ", req.params)
           if (req.path.startsWith("/films")) {
             const title = req.path.split("/")[2];
             if (!title) {
-              console.log(req.path)
-              // res.render("infoTemplate", {results: db.results, searchFilm: true});
+              res.render("infoTemplate");
             } else {
-              // console.log("Parametro buscado:", req.query.searchFilm)
+              console.log("Parametro buscado:", req.query.searchFilm)
               let param:string = String(req.query.searchFilm);
-              // res.render("infoTemplate",{results: db.results.filter(film => (film.title).toUpperCase().includes(param.toUpperCase()))})
+              //{results: db.results.filter(film => (film.title).toUpperCase().includes(param.toUpperCase()))}
+              res.render("infoTemplate")
             }
           } else {
             switch (req.path) {
