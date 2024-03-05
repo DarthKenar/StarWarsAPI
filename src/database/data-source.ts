fb5dd5bdaf306d9778e24543fb97c865b1034560
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-05 14:45:36 -0300

    ✨ New Feature: Base de datos     - Modelos creados:         - People         - Films         (✅ relacion manyToMany)     📦 Configuracion de Typescript:         - Quité el modo inicializar variable (estricto) = FALSE             (Al tener id autoincremental no tiene sentido asignarle un valor por defecto)

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,13 +1,22 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { People, Films } from "./entity/Models"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [Films,People],
     migrations: [],
     subscribers: [],
 })
+
+// to initialize the initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+export var inicialize = AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
---
+++ src/database/entity/Models.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from "typeorm"
+
+@Entity()
+export class Films {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    episode_id: number
+
+    @ManyToMany(type => People, people => people.films)
+    @JoinTable()
+    characters: People[]
+
+}
+
+@Entity()
+export class People {
+
+    @PrimaryGeneratedColumn()
+    name: string
+
+    @Column()
+    gender: string
+
+    @Column()
+    species: string
+
+    @ManyToMany(type => Films, film => film.characters)
+    @JoinTable()
+    films: Films[]
+    
+}
