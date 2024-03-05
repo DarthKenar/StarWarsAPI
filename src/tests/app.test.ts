34c49ecc14d97cc1e2fd913a5f74e75a503d062c
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-05 20:46:22 -0300

    ✏️ Fix Typo:

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,6 +1,6 @@
 // app.test.ts
-import { prueba } from '../build/app';
+// import { prueba } from '../build/server';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(prueba(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(prueba(1, 2)).toBe(3);
+// });
