79eb687e83c2687850c3449eb145736925687051
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-06 17:00:47 -0300

    🚧 Work in Progress:  🔨 Refactor Code:  Reducciond e peticiones a la API. Refactor: Si se corta la carga a la base de datos, ahora se continuará llenando sin mostrar un error

--- src/build/server.ts
+++ src/build/server.ts
@@ -4,7 +4,6 @@ import { Request, Response } from 'express';
 import { engine } from 'express-handlebars';
 import { AppDataSource } from "../database/data-source";
 import { Films, People } from "../database/entity/Models";
-import { IntegerType } from "typeorm";
 
 const app = express()
 const AXIOS = require("axios")
@@ -30,71 +29,75 @@ function sendError(res:Response, codeError:number, errorInfo:string){
   });
 };
 
-async function refillDB(res:Response){
-  try {
-    var linksAPI = await AXIOS.get('https://swapi.dev/api/');
-    var filmsAPI = await AXIOS.get(linksAPI.data.films);
-    for(let i = 0; i < filmsAPI.data.results.length; i++){
-      let filmAPI = filmsAPI.data.results[i]
-      let film = new Films()
-      film.title = filmAPI.title
-      console.log(`CREANDO PELICULA NUEVA...${filmAPI.title}`)
-      film.episode_id = filmAPI.episode_id
-      await AppDataSource.manager.save(film)
-      for(let j = 0; j < filmAPI.characters.length; j++){
-        let characterAPI = await AXIOS.get(filmAPI.characters[j]);
-        let peopleRepository = await AppDataSource.getRepository(People)
-        let characterDB = await peopleRepository.findOneBy({name: characterAPI.data.name})
-        if(characterDB){
-          //Si existe entonces solo agregar la pelicula a su repertorio
-          console.log("EL PERSONAJE YA EXISTE")
-          // characterDB.films.push(film)
-        }else{
-          console.log("CREANDO NUEVA PERSONA...")
-          let people = new People()
-          people.idAPI = filmAPI.characters[j].split('/')[filmAPI.characters[j].split('/').length - 2]
-          people.name = characterAPI.data.name
-          people.gender = characterAPI.data.gender
-          try{
-            console.log("Buscando especie...")
-            let specie = await AXIOS.get(characterAPI.data.species[0])
-            people.species = await specie.data.name
-            console.log(`Especie agregada: ${specie.data.name}`)
-          }catch(err){
-            console.error("La especie no se encuentra!")
-            people.species = "undefined"
-            console.log("Especie indefinida Agregada!")
-          }
-          // people.films = [film]
-          console.log("Guardando Personaje...")
-          AppDataSource.manager.save(people)
-          console.log("Personaje Guardado!")
-        }
-      };
-      await AppDataSource.manager.save(film)
-      console.log(`PELICULA ${film.title} GUARDADA!`)
-    }
-    chekedCompleteDB = true; //Se completo la DB
-  } catch (error) {
-    sendError(res,502,'Bad Gateway');
-  }
-}
+
 
 AppDataSource.initialize()
   .then(() => {
+    async function refillDB(res:Response){
+      try {
+        var filmsAPI = await AXIOS.get("https://swapi.dev/api/films");
+        for(let i = 0; i < filmsAPI.data.results.length; i++){
+          let filmAPI = filmsAPI.data.results[i]
+          //obtener pelicula de la base de datos
+          let filmRepository = await AppDataSource.getRepository(Films)
+          let episode_id = await filmRepository.findOneBy({episode_id: filmAPI.episode_id})
+          if(!episode_id){
+            let film = new Films()
+            film.title = filmAPI.title
+            console.log(`CREANDO PELICULA NUEVA...${filmAPI.title}`)
+            film.episode_id = filmAPI.episode_id
+            await AppDataSource.manager.save(film)
+            for(let j = 0; j < filmAPI.characters.length; j++){
+              let characterAPI = await AXIOS.get(filmAPI.characters[j]);
+              let peopleRepository = await AppDataSource.getRepository(People)
+              let characterDB = await peopleRepository.findOneBy({name: characterAPI.data.name})
+              if(characterDB){
+                //Si existe entonces solo agregar la pelicula a su repertorio
+                console.log("EL PERSONAJE YA EXISTE")
+                // characterDB.films.push(film)
+              }else{
+                console.log("CREANDO NUEVA PERSONA...")
+                let people = new People()
+                let characterID = filmAPI.characters[j].split('/')
+                people.idAPI = characterID[characterID.length - 2]
+                people.name = characterAPI.data.name
+                people.gender = characterAPI.data.gender
+                try{
+                  console.log("Buscando especie...")
+                  let specie = await AXIOS.get(characterAPI.data.species[0])
+                  people.species = await specie.data.name
+                  console.log(`Especie agregada: ${specie.data.name}`)
+                }catch(err){
+                  console.error("La especie no se encuentra!")
+                  people.species = "undefined"
+                  console.log("Especie indefinida Agregada!")
+                }
+                // people.films = [film]
+                console.log("Guardando Personaje...")
+                AppDataSource.manager.save(people)
+                console.log("Personaje Guardado!")
+              }
+            };
+            await AppDataSource.manager.save(film)
+            console.log(`PELICULA ${film.title} GUARDADA!`)
+          }
+        }
+        chekedCompleteDB = true; //Se completo la DB
+      } catch (error) {
+        sendError(res,502,'Bad Gateway');
+      }
+    }
     async function checkDB(res:Response) {
-      /*Chequea la base de datos y si no tiene películas las completa con la API externa*/
-      // Condicional, existe información de peliculas en la base de datos.
-      //Obtener lista de peliculas
       console.log(`BANDERA DE DB (checkedDB)--> ${chekedDB}`)
       const filmsRepository = AppDataSource.getRepository(Films)
       const savedFilms = await filmsRepository.find()
-      if(chekedDB === false && savedFilms.length === 0){
+      let filmsAPI = await AXIOS.get("https://swapi.dev/api/films");
+      if(chekedDB === false && savedFilms.length < filmsAPI.data.count){
         console.log("La base de datos se completará")
         chekedDB = true
         console.log(`BANDERA DE DB (checkedDB)--> ${chekedDB}`)
         await refillDB(res);
-      }else{
+      }else if(savedFilms.length < filmsAPI.data.count){
         chekedCompleteDB = true; //Se completo la DB
       }
     }
@@ -102,6 +105,7 @@ AppDataSource.initialize()
     app.use(async (req:Request, res:Response) => {
       console.log(`Metodo: ${req.method}`);
       console.log(`Path: ${req.path}`);
+      checkDB(res);
       switch (req.method) {
         case "GET":
           console.log("req.path: ", req.path);
@@ -135,7 +139,6 @@ AppDataSource.initialize()
             switch (req.path) {
               case "/":
                 console.log("Chequeando DB")
-                checkDB(res);
                 res.render("homeTemplate", {chekedCompleteDB: chekedCompleteDB});
                 break;
               default:
