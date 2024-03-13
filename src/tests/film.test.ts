050f91e58c5740eb1da9cf2c76c651dfaa4ee833
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-13 18:02:56 -0300

    ✔️ Make a test pass: Los test para film pasan correctamente

--- src/tests/film.test.ts
+++ src/tests/film.test.ts
@@ -4,9 +4,9 @@ import { AppDataSource } from "../database/data-source";
 import server from "../build/index"
 const request = supertest(app)
 
-// beforeAll(async () => {
-//   await AppDataSource.initialize();
-// });
+beforeAll(async () => {
+  await AppDataSource.initialize();
+});
 
 describe("GET a la raiz", () => {
     test("/", async ()=>{
@@ -18,28 +18,31 @@ describe("GET a la raiz", () => {
 })
 
 describe("Peticiones GET para routes Film", () => {
-  //Devuelven 200
-  test("Comprueba si devuelve las 6 películas", async ()=>{
+  //200
+  test("delFilmsAll - Comprueba si devuelve las 6 películas", async ()=>{
     let response = await request.get('/film/s/all').expect(200)
     expect(response.body.results).toHaveLength(6)
   })
-  test("Comprueba que se devuelva una película correctamente y esta tenga su valor characters en true, que indica que tiene personajes guardados y relacionados en la base de datos.", async () => {
+  test("getFilmById - Comprueba que se devuelva una película correctamente y esta tenga su valor characters en true, que indica que tiene personajes guardados y relacionados en la base de datos.", async () => {
     let response = await request.get('/film/1').expect(200)
     expect(response.body.film.characters).toBe(true)
   })
-  // WIP
-  // test("Comprueba que devuelva una lista de películas encontradas", async () => {
-  //   let response = await request.get('/film/s/search').expect(404)
-  //   expect(response.body.error).toBe("No se encontró la película 7.")
-  // })
-  //Devuelven Errores
-  test("Comprueba que devuelva el error correcto, si se ingresan letras al pedir la lista de personajes de una película por id", async ()=>{
-    let response = await request.get('/film/asd').expect(400)
-    expect(response.body.error).toBe("La solicitud \"asd\" es incorrecta.")
+  test("getFilmsByName - Comprueba que devuelva una lista con 4 resultados", async () => {
+    let response = await request.get('/film/s/search').query({ searchFilm: 'a'}).expect(200)
+    expect(response.body.results.length).toBe(4)
   })
-  test("Comprueba que devuelva el error correcto al ingresar una película que no existe.", async () => {
+  //4XX
+  test("getFilmById - Comprueba que devuelva el error correcto, si se ingresan letras al pedir la lista de personajes de una película por id", async ()=>{
+    let response = await request.get('/film/asd').expect(400)
+    expect(response.body).toHaveProperty("error")
+  })
+  test("getFilmById - Comprueba que devuelva el error correcto al ingresar una película que no existe.", async () => {
     let response = await request.get('/film/7').expect(404)
-    expect(response.body.error).toBe("No se encontró la película 7.")
+    expect(response.body).toHaveProperty("error")
+  })
+  test("getFilmsByName - Comprueba que devuelva un error en el que no coincide ninguna película con el título buscado", async () => {
+    let response = await request.get('/film/s/search').query({ searchFilm: 'qwerty'}).expect(404)
+    expect(response.body).toHaveProperty("error")
   })
 })
 
