c39322adc814f8c70605f5c6a605a925494eb9dc
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-13 23:47:30 -0300

    🔨 Refactor Code:  Ahora la base de datos se inicializa en data-source en entorno dev pero en test se inicia en los mismos test para tener un mayor control

--- src/build/index.ts
+++ src/build/index.ts
@@ -1,12 +1,10 @@
 import "reflect-metadata"
 import app from "./app"
-import DataBase from "../database/data-source";
 
 const PORT = process.env.PORT || 3000
 let server = app.listen(PORT);
-DataBase.initialize()
-console.log(`Escuchando en puerto http://localhost:${PORT}...`);
 
+console.log(`Escuchando en puerto http://localhost:${PORT}...`)
 export default server;
 
 
