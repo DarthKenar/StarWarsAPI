f7672c43e421b39c5fc0efbac87544844b6aab3f
Federico Notebook <federico_vega22@hotmail.com>
2024-03-17 16:46:20 -0300

    ✔️ Make a test pass: Creación de usuarios y autentificación funciona bien con contraseñas encriptadas

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -14,17 +14,31 @@ beforeAll(async ()=>{
 })
 
 describe("GET a formularios de registro y autentificación", () => {
-    test("/auth/login", async ()=>{
+    test("getLogin - /auth/login", async ()=>{
         await request
         .get('/auth/login')
         .expect('Content-Type', /application\/json/)
         .expect(200)
     })
-    test("/auth/register", async ()=>{
+    test("getRegister - /auth/register", async ()=>{
         await request
         .get("/auth/register")
         .expect('Content-Type', /application\/json/)
         .expect(200)
+    })
+})
+describe("POST en formularios de registro y autentificación con información del usuario", () => {
+    test("postRegister - /auth/register/send", async ()=>{
+        let response = await request.post("/auth/register/send").send({email: 'john@gmail.com',password: 'cabezón1234'})
+        expect(200)
+        expect(response.body).toHaveProperty("message")
+        expect(response.body.message).toContain("El usuario se ha registrado correctamente.")
+    })
+    test("postLogin - /auth/login", async ()=>{
+        let response = await request.post('/auth/login/send').send({email: 'john@gmail.com',password: 'cabezón1234'})
+        expect(200)
+        expect(response.body).toHaveProperty("message")
+        expect(response.body.message).toContain("El usuario ha ingresado correctamente.")
     })
 })
 
